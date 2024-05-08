e8648d991cd70954a70863ea11409b455f56237d
sandeepkumardeshetti <sandeepkumardeshetti@gmail.com>
2024-05-08 10:05:52 +0530

    added section in home

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Articles from "./articles/page";
+import Team from "./components/TeamsFol/Team";
+import Testimonialss from "./components/TestimonialsFol/Testimonialss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <Articles />
+      <Team />
+      <Testimonialss />
     </main>
   );
 }
