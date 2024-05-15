4ca4c68658f768f701e8c006d379d138600b47e0
sandeepkumardeshetti <sandeepkumardeshetti@gmail.com>
2024-05-15 17:20:31 +0530

    added error boundaries updated

---
+++ app/services/error.js
@@ -0,0 +1,20 @@
+"use client"
+
+import { useEffect } from "react"
+
+
+const ErrorBoundary = ({ error,reset}) => {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+  return (
+    <div>
+      Errore in services
+      
+      <button onClick={reset}>Try again</button>
+    </div>
+  )
+}
+
+export default ErrorBoundary
